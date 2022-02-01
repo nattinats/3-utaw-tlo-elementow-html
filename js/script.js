9f164405fcecec7e5d0b9ebda088e97f5165744b
nattinats <n.szajner@gmail.com>
2022-02-01 22:49:07 +0100

    poprawka

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ const setBackground = () => {
   let p1 = document.querySelector('.first');
   let p2 = document.querySelector('.last');
 
-  p1.style.setBackgroundColor = 'red';
-  p2.style.setBackgroundColor = 'yellow';
+  p1.style.BackgroundColor = 'red';
+  p2.style.BackgroundColor = 'yellow';
 
 /*   p1.classList.toggle('bg-red');
   p2.classList.toggle('bg-yellow'); */
